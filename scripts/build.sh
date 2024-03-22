f476a4652d0a046af65dc9a7d2b7c2ca62d8896f
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2024-03-22 14:37:42 +0100

    enabled unit tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ PATH=$PATH:$DLC:$DLC/bin; export PATH
 
 # if DLC need to be set to anything other than $DLC append below: -DDLC=/usr/dlc 
 if [ "$EXECUTE_UNITTESTS" = "true" ]; then
-  unittestExpr="-DexecuteUnittests=${EXECUTE_UNITTESTS}"
+  unittestExpr="-DexecuteUnittests=true"
 fi
 
 ${ANT_HOME}/bin/ant -f ${scriptpath}/../build.xml -lib /usr/dlc/pct ${unittestExpr}
